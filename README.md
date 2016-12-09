e8fb94ebd20db0e4ea6b9f96655b39739e540adc
Airton <jmoreira@iget.com.br>
2016-12-09 20:58:48 -0300

    Inicializando projeto

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# InstaladorLaravelWindows
+# Projeto Laravel Blank #
+
+Servidor LAMP (Linux, Apache, MySQL, PHP)
+
+Configuração do Vagrant (com provisionamento em Shell Script) para criar uma máquina virtual (Ubuntu Server 14.04 64 Bits) de desenvolvimento em PHP, com o Laravel Framework instalado.
+
+### Pacotes Inclusos ###
+- PHP 7.0
+- MySQL 5.5
+- Git
+- PhpMyAdmin 
+- Composer
+- cURL
+- Vim
+(Para mais detalhes consulte arquivo setup.sh)
+
+
+## Requisitos Mínimos ##
+
+- Virtualbox - https://www.virtualbox.org/
+- Vagrant - http://www.vagrantup.com/
+- Git - http://git-scm.com ( Opicional )
+- Acesso Internet
+
+## Passo a Passo ##
+
+1. Instale o VirtualBox e em seguida o Vagrant
+
+2. Clone o projeto através do comando `git clone https://github.com/jmoreirafilho/InstaladorLaravelWindows.git`
+
+3. Dentro da pasta do projeto, pelo Terminal, execute o comando `vagrant up`
+	- Esse comando, quando executado pela primeira vez pode demorar mais de uma hora, pois irá:
+		- Baixar a VM Ubuntu
+		- Instalar o php
+		- Instalar o mysql e o phpmyadmin
+		- Instalar o Laravel na sua ultima versão
+		- Baixar todas as libs do Laravel
+		- Gerar o Application Key do projeto
+		- Mapear o projeto no apache
+		- Resetar o apache
+
+4. Acesse o link ()
+
+5. Após o comando `vagrant up` terminar de executar, você poderá acessar a sua maquina através do terminal via ssh, digitando `vagrant ssh`
---
+++ setup.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+clear
+echo "\n---- Iniciando instalacao do ambiente de Desenvolvimento PHP com Laravel Framework ---"
+
+echo "\n\n--- Atualizando lista de pacotes ---"
+sudo apt-get update
+
+echo "\n\n--- Definindo Senha padrao para o MySQL e suas ferramentas ---"
+
+DEFAULTPASS="vagrant"
+sudo debconf-set-selections <<EOF
+mysql-server	mysql-server/root_password password $DEFAULTPASS
+mysql-server	mysql-server/root_password_again password $DEFAULTPASS
+dbconfig-common	dbconfig-common/mysql/app-pass password $DEFAULTPASS
+dbconfig-common	dbconfig-common/mysql/admin-pass password $DEFAULTPASS
+dbconfig-common	dbconfig-common/password-confirm password $DEFAULTPASS
+dbconfig-common	dbconfig-common/app-password-confirm password $DEFAULTPASS
+phpmyadmin		phpmyadmin/reconfigure-webserver multiselect apache2
+phpmyadmin		phpmyadmin/dbconfig-install boolean true
+phpmyadmin      phpmyadmin/app-password-confirm password $DEFAULTPASS 
+phpmyadmin      phpmyadmin/mysql/admin-pass     password $DEFAULTPASS
+phpmyadmin      phpmyadmin/password-confirm     password $DEFAULTPASS
+phpmyadmin      phpmyadmin/setup-password       password $DEFAULTPASS
+phpmyadmin      phpmyadmin/mysql/app-pass       password $DEFAULTPASS
+EOF
+
+echo "\n\n--- Instalando pacotes basicos ---"
+sudo apt-get install vim curl python-software-properties git-core --assume-yes --force-yes
+
+echo "\n\n--- Instalando MySQL, Phpmyadmin e alguns outros modulos ---"
+sudo apt-get install mysql-server-5.7 mysql-client phpmyadmin --assume-yes --force-yes
+
+echo "\n\n--- Instalando PHP, Apache e alguns modulos ---"
+sudo apt-get install php7.0 apache2 libapache2-mod-php7.0 php7.0-curl php7.0-gd php7.0-mcrypt  php7.0-mysql --assume-yes --force-yes
+
+echo "\n\n--- Habilitando mod-rewrite do Apache ---"
+sudo a2enmod rewrite
+
+echo "\n\n--- Reiniciando Apache ---"
+sudo service apache2 restart
+
+echo "\n\n--- Baixando e Instalando Composer ---"
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+# Instale apartir daqui o que você desejar 
+echo "\n\n--- Criando Projeto Laravel ---"
+sudo chmod -R 777 /www/
+sudo composer create-project --prefer-dist laravel/laravel /www/laravel
+
+echo "\n\n--- Configurando Mapeamento do Projeto ---"
+sudo cp /www/config/laravel.conf.tpl /etc/apache2/sites-available/laravel.conf
+sudo ln -s /etc/apache2/sites-available/laravel.conf /etc/apache2/sites-enabled/laravel.conf
+sudo rm -R /etc/apache2/sites-enabled/000-default.conf
+sudo service apache2 restart
+
+echo "\n\n\n\n--- Ambiente de desenvolvimento instalado com sucesso ---"
